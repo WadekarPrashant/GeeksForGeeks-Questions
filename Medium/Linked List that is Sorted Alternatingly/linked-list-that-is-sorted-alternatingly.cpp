1e49d2d7cc25db269b29937a7b318f3f843ba180
Prashant Wadekar <94431847+WadekarPrashant@users.noreply.github.com>
2024-03-15 11:15:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+/* Function to print linked list */
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+/*
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+
+class Solution
+{
+public:
+    void sort(Node **head){
+        Node *list1 = *head;
+        Node *list2 = nullptr;
+        
+        while(list1->next != nullptr) {
+            Node *temp = list1->next->next;
+            list1->next->next = list2;
+            list2 = list1->next;
+            list1->next = temp;
+            list1 = list1->next == nullptr ? list1 : list1->next;
+        } 
+        
+        list1->next = list2;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    Solution ob;
+	    ob.sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
