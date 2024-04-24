0d166c67128757ae885b499995d1deff642c9227
Prashant Wadekar <94431847+WadekarPrashant@users.noreply.github.com>
2024-04-24 23:34:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Paths to reach origin/paths-to-reach-origin.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+public:
+    int ways(int n, int m) {
+        const int MOD = 1000000007;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        
+        for (int i = 0; i <= m; i++)
+            dp[0][i] = 1;
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        Solution ob;
+        cout<<ob.ways(x,y)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
