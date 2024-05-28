f996c422accd359114a3ad22be9717e3162a08f5
Prashant Wadekar <94431847+WadekarPrashant@users.noreply.github.com>
2024-05-28 23:27:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum cost to fill given weight in a bag/minimum-cost-to-fill-given-weight-in-a-bag.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int minimumCost(int n, int w, vector<int> &cost) {
+        vector<int> dp(w + 1, INT_MAX);
+        dp[0] = 0;
+    
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= w; j++)
+                dp[j] = min(dp[j], cost[i] + dp[j - i - 1]); 
+    
+        return dp[w];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int w;
+        scanf("%d", &w);
+
+        vector<int> cost(n);
+        Array::input(cost, n);
+
+        Solution obj;
+        int res = obj.minimumCost(n, w, cost);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
